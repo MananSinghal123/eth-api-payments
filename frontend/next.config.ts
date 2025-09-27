10d370ce51056e07c9c3b11e094f2fe4e3f52437
MananSinghal123 <23ucs636@lnmiit.ac.in>
2025-09-27 14:12:03 +0530

    feat:sdk added

--- frontend/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ this disables ESLint blocking during builds
-  },
-};
-
-export default nextConfig;
